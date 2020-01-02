730c978d8cc1a3b63405b816ec6d2e79bd876938
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-01-02 23:42:16 +0530

    Basic setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const exhb=require('express-handlebars');
+const express=require('express');
+const methodoverride=require('method-override');
+const bodyparser=require('body-parser');
+const app=express();
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+app.use(methodoverride("_method"));
+app.engine("hbs",exhb());
+app.set("view engine","hbs");
